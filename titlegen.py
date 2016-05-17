37e990a53e6ae8da1e31b382678011a387830b78
davidecarroll <carroll.davide@gmail.com>
2016-05-17 22:53:17 +0100

    remove script

--- titlegen.py
+++ titlegen.py
@@ -100,7 +100,7 @@ def generate_title():
 
         output.append(tok)
         prev_token = tok
-    
+
     multiplier = num_qualifiers**-1.5
     if has_infix_role:
         multiplier *= 0.9
@@ -138,22 +138,6 @@ PRECONTENT = """<!DOCTYPE html>
 <meta name="og:description" content="Damning indictment of the corporatization of higher education, or career finder? You decide!" />
 <meta name="description" content="Damning indictment of the corporatization of higher education, or career finder? You decide!" />
 <meta name="robots" content="nosnippet" />
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-77758304-1', 'auto');
-  ga('send', 'pageview');
-</script>
-<script>
-var trackOutboundLink = function(url) {
-   ga('send', 'event', 'outbound', 'click', url, {
-     'transport': 'beacon',
-     'hitCallback': function(){document.location = url;}
-   });
-}
-</script>
 
 </head><body>
 <div id="maincontent">
